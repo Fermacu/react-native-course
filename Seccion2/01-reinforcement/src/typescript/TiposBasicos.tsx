952ffe079caec027e2407a7ebe9a24f0de89e893
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-13 21:50:58 -0600

    refactor: improving of TiposBasicos📦

--- Seccion2/01-reinforcement/src/typescript/TiposBasicos.tsx
+++ Seccion2/01-reinforcement/src/typescript/TiposBasicos.tsx
@@ -1,7 +1,18 @@
 export const TiposBasicos = () => {
+
+    const nombre: string = 'Fernando';
+    const edad: number = 35
+    const estaActivo: boolean = false
+
+    const poderes: string[] = []
+
+
   return (
     <>
     <h3>Tipos básicos</h3>
+    {nombre}, {edad}, {(estaActivo) ? 'Activo' : 'No activo'}
+    <br/>
+    {poderes.join(', ')}
     </>
   )
 }
